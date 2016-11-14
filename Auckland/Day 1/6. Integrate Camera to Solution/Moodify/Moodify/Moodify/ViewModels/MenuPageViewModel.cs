4c23f41d928a39bb7a178e60115b661481a3033c
Jason Goodsell <jasongoodsell@studentpartner.com>
2016-11-14 22:19:32 +1300

    Menu view model added.

--- Auckland/Day 1/6. Integrate Camera to Solution/Moodify/Moodify/Moodify/ViewModels/MenuPageViewModel.cs
+++ Auckland/Day 1/6. Integrate Camera to Solution/Moodify/Moodify/Moodify/ViewModels/MenuPageViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Moodify.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Moodify.ViewModels
 {
