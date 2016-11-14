dd07f3039bbf35cc6b7ff51efa6fa8697ea83233
Jason Goodsell <jasongoodsell@studentpartner.com>
2016-11-14 22:23:25 +1300

    Side menu added.

---
+++ Auckland/Day 1/6. Integrate Camera to Solution/Moodify/Moodify/Moodify/Views/MenuPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Moodify.Views
+{
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            BindingContext = new MenuPageViewModel();
+            Title = "Menu";
+            Icon = Device.OS == TargetPlatform.iOS ? "menu.png" : null;
+            InitializeComponent();
+        }
+    }
+}
