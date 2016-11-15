4e80fe6315dd7ee0eacb291e35afbc041a6fcb0b
Jason Goodsell <jasongoodsell@studentpartner.com>
2016-11-16 12:28:45 +1300

    Completed project added.

---
+++ Auckland/Day 2/Weather Bot/Completed project/Weather Bot/Controllers/MessagesController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using Weather_Bot.Models;
+
+namespace Weather_Bot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                WeatherObject.RootObject rootObject;
+
+                Console.WriteLine(activity.Attachments[0].ContentUrl);
+
+                HttpClient client = new HttpClient();
+                string x = await client.GetStringAsync(new Uri("http://api.openweathermap.org/data/2.5/weather?q=" + activity.Text + "&units=metric&APPID=440e3d0ee33a977c5e2fff6bc12448ee"));
+
+                rootObject = JsonConvert.DeserializeObject<WeatherObject.RootObject>(x);
+
+                string cityName = rootObject.name;
+                string temp = rootObject.main.temp + "°C";
+                string pressure = rootObject.main.pressure + "hPa";
+                string humidity = rootObject.main.humidity + "%";
+                string wind = rootObject.wind.deg + "°";
+
+                // return our reply to the user
+                Activity reply = activity.CreateReply($"Current weather for {cityName} is {temp}, pressure {pressure}, humidity {humidity}, and wind speeds of {wind}");
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
