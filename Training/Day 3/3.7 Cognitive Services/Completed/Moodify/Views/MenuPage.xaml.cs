2e961ceb53307a9f4eaa148c3e9f9e87ae768478
Jason Goodsell <jasongoodsell@studentpartner.com>
2016-11-20 21:20:12 +1300

    Completed project added.

---
+++ Training/Day 3/3.7 Cognitive Services/Completed/Moodify/Views/MenuPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Moodify
+{
+	public partial class MenuPage : ContentPage
+	{
+		public MenuPage()
+		{
+			BindingContext = new MenuPageViewModel();
+			Title = "Menu";
+            Icon = Device.OS == TargetPlatform.iOS ? "menu.png" : null;
+            InitializeComponent();
+		}
+	}
+}
